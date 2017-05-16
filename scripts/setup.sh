6b71bd5962b4bd3047df8ba6c5460bc12bbe7961
Simone Pigazzi <simone.pigazzini@live.it>
2017-05-16 17:45:47 +0200

    Better job handling

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
-cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.05.02/x86_64-slc6-gcc49-opt/root/
+cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.14/x86_64-slc6-gcc49-opt/root/
 source ./bin/thisroot.sh
 cd -
+source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
 
-export LD_LIBRARY_PATH=DynamicTTree/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=DynamicTTree/lib/:CfgManager/lib/:$LD_LIBRARY_PATH
