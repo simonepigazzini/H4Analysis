53eec5cca332ded04f8998249d91a89a274edb83
Simone Pigazzini <simone.pigazzini@cern.ch>
2018-06-01 11:11:20 +0200

    Fixed Amplitude and Phase computation in FFTClass

--- src/FFTClass.cc
+++ src/FFTClass.cc
@@ -50,8 +50,19 @@ const vector<double>* FFTClass::GetPhases()
     //---first check if phases vector is already full
     //   if not fill it
     if(vars_["phase"].size() == 0)
+    {
         for(unsigned int i=0; i<vars_["re"].size(); ++i)
-            vars_["phase"].push_back(atan(vars_["im"][i]/vars_["re"][i]));
+        {
+            //---note that re==0 && im==0 is not contemplated.
+            float sign = (vars_["im"][i]>0 ? 1 : -1);
+            if(vars_["re"][i] == 0)
+                vars_["phase"].push_back(sign*TMath::Pi()/2.);
+            else if(vars_["re"][i] > 0)
+                vars_["phase"].push_back(atan(vars_["im"][i]/vars_["re"][i]));
+            else
+                vars_["phase"].push_back(atan(vars_["im"][i]/vars_["re"][i])+sign*TMath::Pi());
+        }
+    }
 
     return &vars_["phase"];
 }
