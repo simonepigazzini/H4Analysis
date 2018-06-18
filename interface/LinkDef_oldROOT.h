69ed4bcede9cbf0c0cda455594fe686d3d17ee06
Simone Pigazzini <simone.pigazzini@cern.ch>
2018-06-19 00:09:13 +0200

    Update CfgManager and clean up multiple LinkDef.h files

--- interface/LinkDef_oldROOT.h
+++
@@ -1,8 +0,0 @@
-#include "WFClass.h"
-#include "WFViewer.h"
-
-#ifdef __CLING__
-#pragma link C++ class InterpolatorFunc+;
-#pragma link C++ class WFViewer+;
-#pragma link C++ class MCPAnalyzer+;
-#endif
