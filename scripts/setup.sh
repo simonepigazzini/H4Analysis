ef182a199ef68a0c540d7d35a935a7dbd07f9501
Simone Pigazzi <simone.pigazzini@live.it>
2019-04-26 14:18:59 +0200

    slc7 setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/ROOT/6.14.04/x86_64-slc6-gcc8-opt/ROOT-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/ROOT/6.14.04/x86_64-centos7-gcc8-opt/ROOT-env.sh
 
 export LD_LIBRARY_PATH=./lib:DynamicTTree/lib/:CfgManager/lib/:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=./interface:DynamicTTree/interface/:CfgManager/interface/:$ROOT_INCLUDE_PATH
