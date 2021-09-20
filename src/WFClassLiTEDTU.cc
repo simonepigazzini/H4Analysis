4c2e9c43c335146f6b8073ce166343b3ca4e3e94
chiara amendola <chiara.amendola@cern.ch>
2021-09-20 15:32:37 +0200

    minor

--- src/WFClassLiTEDTU.cc
+++ src/WFClassLiTEDTU.cc
@@ -26,7 +26,7 @@ WFFitResults WFClassLiTEDTU::TemplateFit(float amp_threshold, float offset, int 
     double tmplFitChi2=0;
     if(tmplFitAmp_ == -1)
      {
-        amp_threshold *= gain_;
+        amp_threshold *= gain_[maxSample_];
         if(samples_[maxSample_]>amp_threshold)
         {
             //---set template fit window around maximum, [min, max)
